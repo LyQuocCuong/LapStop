cb3f5c9f3433aec32c8cd24bc4ae8024793f0a39
Cuong Ly <quoccuongit95@gmail.com>
2022-10-10 23:40:00 +0700

    [Feature/OnionArchitecture] - Step 1.0 - Build Employee's models

---
+++ LapStopApiSolution/Entities/Models/EmployeeRole.cs
@@ -0,0 +1,20 @@
+﻿using LapStopApi.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LapStopApi.Models
+{
+    public class EmployeeRole
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActivate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ LapStopApiSolution/Entities/Models/EmployeeStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LapStopApi.Entities.Models
+{
+    public class EmployeeStatus
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActivate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
