af90bf08b12a556258ba84cbdb633d61e83b369b
Cuong Ly <quoccuongit95@gmail.com>
2023-04-02 11:09:32 +0700

    [feature] Config DeleteBehavior for all models

--- LapStopApiSolution/Entities/Extensions/ModelBuilderExt.cs
+++ LapStopApiSolution/Entities/Extensions/ModelBuilderExt.cs
@@ -68,7 +68,8 @@ namespace Entities.Extensions
             modelBuilder.Entity<Brand>()
                 .HasMany<ProductBrand>(b => b.ProductsOfABrand)
                 .WithOne(pb => pb.Brand)
-                .HasForeignKey(pb => pb.BrandId);
+                .HasForeignKey(pb => pb.BrandId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -78,7 +79,8 @@ namespace Entities.Extensions
             modelBuilder.Entity<Cart>()
                 .HasMany<CartItem>(c => c.CartItems)
                 .WithOne(ci => ci.Cart)
-                .HasForeignKey(ci => ci.CartId);
+                .HasForeignKey(ci => ci.CartId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -88,19 +90,22 @@ namespace Entities.Extensions
             modelBuilder.Entity<Customer>()
                 .HasOne<Cart>(cus => cus.Cart)
                 .WithOne(c => c.Customer)
-                .HasForeignKey<Cart>(c => c.CustomerId);
+                .HasForeignKey<Cart>(c => c.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) Customer <--> (1) CustomerAccount
             modelBuilder.Entity<Customer>()
                 .HasOne<CustomerAccount>(c => c.CustomerAccount)
                 .WithOne(ca => ca.Customer)
-                .HasForeignKey<CustomerAccount>(ca => ca.CustomerId);
+                .HasForeignKey<CustomerAccount>(ca => ca.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) Customer --> (N) SalesOrder
             modelBuilder.Entity<Customer>()
                 .HasMany<SalesOrder>(cus => cus.SalesOrders)
                 .WithOne(so => so.Customer)
-                .HasForeignKey(so => so.CustomerId);
+                .HasForeignKey(so => so.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -116,19 +121,22 @@ namespace Entities.Extensions
             modelBuilder.Entity<Employee>()
                 .HasMany<EmployeeGallery>(e => e.EmployeeGalleries)
                 .WithOne(eg => eg.Employee)
-                .HasForeignKey(eg => eg.EmployeeId);
+                .HasForeignKey(eg => eg.EmployeeId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) Employee --> (N) ImportedInvoice
             modelBuilder.Entity<Employee>()
                 .HasMany<ImportedInvoice>(e => e.ImportedInvoices)
                 .WithOne(ii => ii.Employee)
-                .HasForeignKey(ii => ii.EmployeeId);
+                .HasForeignKey(ii => ii.EmployeeId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) Employee --> (N) ExportedInvoice
             modelBuilder.Entity<Employee>()
                 .HasMany<ExportedInvoice>(e => e.ExportedInvoices)
                 .WithOne(ei => ei.Employee)
-                .HasForeignKey(ei => ei.EmployeeId);
+                .HasForeignKey(ei => ei.EmployeeId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -138,7 +146,8 @@ namespace Entities.Extensions
             modelBuilder.Entity<EmployeeRole>()
                 .HasMany<Employee>(er => er.Employees)
                 .WithOne(e => e.EmployeeRole)
-                .HasForeignKey(e => e.EmployeeRoleId);
+                .HasForeignKey(e => e.EmployeeRoleId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -148,7 +157,8 @@ namespace Entities.Extensions
             modelBuilder.Entity<EmployeeStatus>()
                 .HasMany<Employee>(es => es.Employees)
                 .WithOne(e => e.EmployeeStatus)
-                .HasForeignKey(e => e.EmployeeStatusId);
+                .HasForeignKey(e => e.EmployeeStatusId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -158,7 +168,8 @@ namespace Entities.Extensions
             modelBuilder.Entity<ExportedInvoice>()
                 .HasMany<ExportedInvoiceDetail>(ei => ei.ExportedInvoiceDetails)
                 .WithOne(eid => eid.ExportedInvoice)
-                .HasForeignKey(eid => eid.ExportedInvoiceId);
+                .HasForeignKey(eid => eid.ExportedInvoiceId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -168,7 +179,8 @@ namespace Entities.Extensions
             modelBuilder.Entity<ImportedInvoice>()
                 .HasMany<ImportedInvoiceDetail>(ip => ip.ImportedInvoiceDetails)
                 .WithOne(iid => iid.ImportedInvoice)
-                .HasForeignKey(iid => iid.ImportedInvoiceId);
+                .HasForeignKey(iid => iid.ImportedInvoiceId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -178,13 +190,15 @@ namespace Entities.Extensions
             modelBuilder.Entity<InvoiceStatus>()
                 .HasMany<ImportedInvoice>(ins => ins.ImportedInvoices)
                 .WithOne(ii => ii.InvoiceStatus)
-                .HasForeignKey(ii => ii.InvoiceStatusId);
+                .HasForeignKey(ii => ii.InvoiceStatusId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) InvoiceStatus --> (N) ExportedInvoice
             modelBuilder.Entity<InvoiceStatus>()
                 .HasMany<ExportedInvoice>(ins => ins.ExportedInvoices)
                 .WithOne(ei => ei.InvoiceStatus)
-                .HasForeignKey(ei => ei.InvoiceStatusId);
+                .HasForeignKey(ei => ei.InvoiceStatusId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -194,37 +208,43 @@ namespace Entities.Extensions
             modelBuilder.Entity<Product>()
                 .HasMany<ProductBrand>(p => p.BrandsOfAProduct)
                 .WithOne(boap => boap.Product)
-                .HasForeignKey(boap => boap.ProductId);
+                .HasForeignKey(boap => boap.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) Product --> (N) ProductGallery
             modelBuilder.Entity<Product>()
                 .HasMany<ProductGallery>(p => p.ProductGalleries)
                 .WithOne(pg => pg.Product)
-                .HasForeignKey(pg => pg.ProductId);
+                .HasForeignKey(pg => pg.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) Product --> (N) CartItem
             modelBuilder.Entity<Product>()
                 .HasMany<CartItem>(p => p.CartItems)
                 .WithOne(ci => ci.Product)
-                .HasForeignKey(ci => ci.ProductId);
+                .HasForeignKey(ci => ci.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) Product --> (N) SalesOrderDetail
             modelBuilder.Entity<Product>()
                 .HasMany<SalesOrderDetail>(p => p.SalesOrderDetails)
                 .WithOne(sod => sod.Product)
-                .HasForeignKey(sod => sod.ProductId);
+                .HasForeignKey(sod => sod.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) Product --> (N) ExportedInvoiceDetail
             modelBuilder.Entity<Product>()
                 .HasMany<ExportedInvoiceDetail>(p => p.ExportedInvoiceDetails)
                 .WithOne(eid => eid.Product)
-                .HasForeignKey(eid => eid.ProductId);
+                .HasForeignKey(eid => eid.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) Product --> (N) ImportedInvoiceDetail
             modelBuilder.Entity<Product>()
                 .HasMany<ImportedInvoiceDetail>(p => p.ImportedInvoiceDetails)
                 .WithOne(iid => iid.Product)
-                .HasForeignKey(iid => iid.ProductId);
+                .HasForeignKey(iid => iid.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -234,7 +254,8 @@ namespace Entities.Extensions
             modelBuilder.Entity<ProductStatus>()
                 .HasMany<Product>(ps => ps.Products)
                 .WithOne(p => p.ProductStatus)
-                .HasForeignKey(p => p.ProductStatusId);
+                .HasForeignKey(p => p.ProductStatusId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -244,13 +265,15 @@ namespace Entities.Extensions
             modelBuilder.Entity<SalesOrder>()
                 .HasOne<ExportedInvoice>(so => so.ExportedInvoice)
                 .WithOne(ei => ei.SalesOrder)
-                .HasForeignKey<ExportedInvoice>(ei => ei.SalesOrderId);
+                .HasForeignKey<ExportedInvoice>(ei => ei.SalesOrderId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             // (1) SalesOrder --> (N) SalesOrderDetail
             modelBuilder.Entity<SalesOrder>()
                 .HasMany<SalesOrderDetail>(so => so.SalesOrderDetails)
                 .WithOne(sod => sod.SalesOrder)
-                .HasForeignKey(sod => sod.SalesOrderId);
+                .HasForeignKey(sod => sod.SalesOrderId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
@@ -260,7 +283,8 @@ namespace Entities.Extensions
             modelBuilder.Entity<SalesOrderStatus>()
                 .HasMany<SalesOrder>(sos => sos.SalesOrders)
                 .WithOne(so => so.SalesOrderStatus)
-                .HasForeignKey(so => so.SalesOrderStatusId);
+                .HasForeignKey(so => so.SalesOrderStatusId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             #endregion
 
