cc43abea42a54941a7200b4f7e0c2319e04b0a74
Cuong Ly <quoccuongit95@gmail.com>
2023-04-03 18:21:34 +0700

    [enhancement][MIGRATION] add IsEnable field to ProductStatus

--- LapStopApiSolution/Entities/Migrations/20230403110644_ProductStatus_add_IsEnable_field.Designer.cs
+++ LapStopApiSolution/Entities/Migrations/20230403110644_ProductStatus_add_IsEnable_field.Designer.cs
@@ -4,6 +4,7 @@ using Entities.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Entities.Migrations
 {
     [DbContext(typeof(LapStopContext))]
-    partial class LapStopContextModelSnapshot : ModelSnapshot
+    [Migration("20230403110644_ProductStatus_add_IsEnable_field")]
+    partial class ProductStatus_add_IsEnable_field
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LapStopApiSolution/Entities/Migrations/20230403110644_ProductStatus_add_IsEnable_field.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductStatus_add_IsEnable_field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnable",
+                table: "ProductStatus",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEnable",
+                table: "ProductStatus");
+        }
+    }
+}
