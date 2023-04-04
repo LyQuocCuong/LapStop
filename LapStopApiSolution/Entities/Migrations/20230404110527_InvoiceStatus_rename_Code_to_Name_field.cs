b6963abf477af6a8adc5d6c0f2bf3914218e819c
Cuong Ly <quoccuongit95@gmail.com>
2023-04-04 18:12:13 +0700

    [enhancement][migration] InvoiceStatus table renames Code to Name field

---
+++ LapStopApiSolution/Entities/Migrations/20230404110527_InvoiceStatus_rename_Code_to_Name_field.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class InvoiceStatus_rename_Code_to_Name_field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Code",
+                table: "InvoiceStatus",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "InvoiceStatus",
+                newName: "Code");
+        }
+    }
+}
