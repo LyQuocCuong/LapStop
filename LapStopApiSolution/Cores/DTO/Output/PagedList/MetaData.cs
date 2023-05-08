09d7a564f9498ec7cc02349925a0f94eed6c4e87
Cuong Ly <quoccuongit95@gmail.com>
2023-05-08 17:56:15 +0700

    [Enhancement] Improve Paging feature

---
+++ LapStopApiSolution/Cores/DTO/Output/PagedList/MetaData.cs
@@ -0,0 +1,17 @@
+﻿namespace DTO.Output.PagedList
+{
+    public class MetaData
+    {
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalRecords { get; set; }
+
+        public bool HasPrevious => CurrentPage > 1;
+
+        public bool HasNext => CurrentPage < TotalPages;
+    }
+}
---
+++ LapStopApiSolution/Cores/DTO/Output/PagedList/PagedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Output.PagedList
+{
+    public sealed class PagedList<T> : List<T>
+    {
+        public MetaData MetaData { get; set; }
+
+        public PagedList(List<T> sourceData, int totalRecords, int pageNumber, int pageSize) 
+        {
+            MetaData = new MetaData()
+            {
+                TotalRecords = totalRecords,
+                PageSize = pageSize,
+                CurrentPage = pageNumber,
+                TotalPages = (int)Math.Ceiling(totalRecords / (double)pageSize),
+            };
+
+            AddRange(sourceData);
+        }
+    }
+}
